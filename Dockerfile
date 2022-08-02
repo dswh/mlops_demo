84282c975cb705048472476b9c99ce3b40a9d96d
Data Science with Harshit <61068902+dswh@users.noreply.github.com>
2022-08-02 17:23:57 +0530

    Update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 
-CMD ["flask", "run", "--host=0.0.0.0"]
+CMD ["python", "-m", "flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -55,7 +55,3 @@ def prediction():
 @app.route('/ping', methods=['GET'])
 def ping():
     return "Pinging Model!!"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
